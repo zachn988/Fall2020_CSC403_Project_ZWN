f777a44b50a88c885765213b0a0927f9ba771888
ZachNalepa <zachnalepa@gmail.com>
2023-10-20 14:41:38 -0500

    enemy spawn locations and timer

--- CSC 403 Game/map_1.gd
+++ CSC 403 Game/map_1.gd
@@ -1,10 +1,23 @@
 extends TileMap
 
+@export var enemyScene: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	new_game()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _on_mob_timer_timeout():
+	var enemy = enemyScene.instantiate()
+	var enemySpawnLocation = get_node("enemyPath/enemySpawns")
+	enemySpawnLocation.progress_ratio = randf()
+	var direction = enemySpawnLocation.rotation + PI/2
+	enemy.position = enemySpawnLocation.position
+	add_child(enemy)
+	
+func new_game():
+	$enemyTimer.start()
